304883f4dc6bb93e11fecb91afaad1800bac23f8
Oskari Järvinen <Oskari Jrvinen>
2021-03-16 00:44:22 +0200

    Ammended comments

--- index.js
+++ index.js
@@ -237,60 +237,34 @@ function createNote(noteID, x, y, tvalue, sx = "60px", sy = "40px") {
   }
 }
 
-/**
- * Makes an element draggable.
- *
- * @param {HTMLElement} element - The element.
- */
 function draggable(element) {
 	var isMouseDown = false;
 
-    // initial mouse X and Y for `mousedown`
     var mouseX = 0;
     var mouseY = 0;
 
-    // element X and Y before and after move
     var elementX = element.style.left;
     var elementY = element.style.top;
 
-	// mouse button down over the element
     element.addEventListener('mousedown', onMouseDown);
 
-	/**
-     * Listens to `mousedown` event.
-     *
-     * @param {Object} event - The event.
-     */
+
 	function onMouseDown(event) {
         mouseX = event.clientX;
         mouseY = event.clientY;
         isMouseDown = true;
     }
 
-	// mouse button released
     element.addEventListener('mouseup', onMouseUp);
 
-	/**
-     * Listens to `mouseup` event.
-     *
-     * @param {Object} event - The event.
-     */
 	function onMouseUp(event) {
         isMouseDown = false;
         elementX = parseInt(element.style.left) || 0;
         elementY = parseInt(element.style.top) || 0;
     }
 
-	// need to attach to the entire document
-    // in order to take full width and height
-    // this ensures the element keeps up with the mouse
     document.addEventListener('mousemove', onMouseMove);
-
-	/**
-     * Listens to `mousemove` event.
-     *
-     * @param {Object} event - The event.
-     */
+    
 	function onMouseMove(event) {
     	if (!isMouseDown) return;
         var deltaX = event.clientX - mouseX;
